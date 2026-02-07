29f37325104578f3e259dd99b7da2a07567d851a
Nagmani Jha <nagmanijha31@gmail.com>
2026-02-07 21:31:56 +0530

    fix(backend): correct CORS and Cookie settings for production

--- src/index.ts
+++ src/index.ts
@@ -28,25 +28,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
+  cors({
+    origin: config.FRONTEND_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(
   session({
     name: "session",
     keys: [config.SESSION_SECRET],
     maxAge: 24 * 60 * 60 * 1000,
     secure: config.NODE_ENV === "production",
     httpOnly: true,
-    sameSite: "lax",
+    sameSite: config.NODE_ENV === "production" ? "none" : "lax",
   })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
-
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
 
 app.get(
   `/`,
